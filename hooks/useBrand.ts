60011503b31f0a4bd614a3dfb889bab5152bf603
cesine <cesine@yahoo.com>
2022-02-18 14:13:36 +0200

    customize 404 to instruct you on how to get into one of the brands

--- hooks/useBrand.ts
+++ hooks/useBrand.ts
@@ -1,8 +1,24 @@
 import Cookies from 'js-cookie';
 import { COOKIE_NAME } from '@lib/constants';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export const useBrand = () => {
   const brand = Cookies.get(COOKIE_NAME) || '';
+  const router = useRouter();
+
+  useEffect(() => {
+    const { pathname, search } = window.location;
+    if (!router.asPath.indexOf('brand')) {
+      return;
+    }
+    // remove the brand from the query params
+    let query = new URLSearchParams(window.location.search);
+    query.delete('brand');
+
+    // TODO this causes infinite redirect loops
+    // router.replace(router.pathname.replace(`/${brand}`, ''), query.toString(), { shallow: true });
+  }, [brand, router]);
 
   return brand;
 }
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,33 @@
-import { useRouter } from 'next/router'
+import Head from 'next/head'
+import { Layout, Link, Page, Text, Button } from '@vercel/examples-ui'
+import { useBrand } from '@hooks/useBrand';
 
 export default function Custom404() {
-  const router = useRouter()
-  const brand = router.query.brand || '' as string
+  const brand = useBrand();
 
+  return (
+  <Page>
+    <Head>
+      <title>${brand} Not Found</title>
+      <meta
+        name="description"
+        content="Vercel example how to use multi-brand custom not found"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <section className="flex flex-col gap-6">
+      <Text variant="h1">404 - {brand.toUpperCase()} Not Found</Text>
+      <Text>You can use a query parameter or a cookie to view one of the avilable brands</Text>
 
-  return <h1>404 - {brand.toUpperCase()} Not Found</h1>
+      <Text className="text-lg mb-4">
+        <Link href='/?brand=chene'>Chene</Link>
+      </Text>
+      <Text className="text-lg mb-4">
+        <Link href='/?brand=sapin'>Sapin</Link>
+      </Text>
+    </section>
+  </Page>);
 }
+
+
+Custom404.Layout = Layout;
