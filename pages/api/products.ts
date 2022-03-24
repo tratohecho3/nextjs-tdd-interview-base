e1f01fb6e0bfcae9aa249c794a724b9b0593089e
cesine <cesine@yahoo.com>
2022-03-24 11:57:24 -0400

    add some images

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,18 +4,22 @@ const products = [{
   id: '123-abc',
   name: 'flooring',
   brand: 'chene',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-731352850.jpg'
 }, {
   id: '456-efg',
   name: 'tree farm',
   brand: 'sapin',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-1932235397.jpg'
 }, {
   id: '456-efg',
   name: 'hiking',
   brand: 'sapin',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-41031721.jpg'
 }, {
   id: '789-hjk',
   name: 'sugar',
   brand: 'palmier',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-563268664.jpg'
 }];
 
 /**
