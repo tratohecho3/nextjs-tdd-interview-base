2c08d2ec83f763155ae431f61c8318891513171d
cesine <cesine@users.noreply.github.com>
2022-05-10 13:28:51 -0400

    add images

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,20 +2,24 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 const products = [{
   id: '123-abc',
-  name: 'flooring',
+  name: '2in flooring',
   brand: 'chene',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-731352850.jpg'
 }, {
   id: '456-efg',
-  name: 'tree farm',
+  name: 'spruce',
   brand: 'sapin',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-1932235397.jpg'
 }, {
-  id: '456-efg',
-  name: 'hiking',
+  id: '567-ihj',
+  name: 'fur',
   brand: 'sapin',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-41031721.jpg'
 }, {
   id: '789-hjk',
-  name: 'sugar',
+  name: 'Date Palm',
   brand: 'palmier',
+  thumbnail: 'https://image.shutterstock.com/image-photo/-260nw-563268664.jpg'
 }];
 
 /**
